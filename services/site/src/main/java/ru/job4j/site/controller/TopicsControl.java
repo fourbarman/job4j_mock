865e29b1f7b2cd1d950a660def7e90ee2341a9c4
fourbarman <maks.java@yandex.ru>
2024-05-30 18:30:07 +0400

    Added interviews count to topics template.

--- services/site/src/main/java/ru/job4j/site/controller/TopicsControl.java
+++ services/site/src/main/java/ru/job4j/site/controller/TopicsControl.java
@@ -8,12 +8,16 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.job4j.site.service.AuthService;
-import ru.job4j.site.service.CategoriesService;
-import ru.job4j.site.service.NotificationService;
-import ru.job4j.site.service.TopicsService;
+import ru.job4j.site.dto.InterviewDTO;
+import ru.job4j.site.dto.TopicDTO;
+import ru.job4j.site.service.*;
 
 import javax.servlet.http.HttpServletRequest;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static ru.job4j.site.controller.RequestResponseTools.getToken;
 
@@ -25,7 +29,7 @@ public class TopicsControl {
     private final TopicsService topicsService;
     private final AuthService authService;
     private final CategoriesService categoriesService;
-
+    private final InterviewsService interviewsService;
     private final NotificationService notifications;
 
     @GetMapping("/{categoryId}")
@@ -33,8 +37,15 @@ public class TopicsControl {
                                 Model model,
                                 HttpServletRequest req) throws JsonProcessingException {
         var topics = topicsService.getByCategory(categoryId);
+        List<InterviewDTO> interviewsByType = interviewsService.getByType(1);
+
+        Map<Integer, List<InterviewDTO>> interviewsByTopicMap
+                = interviewsByTopicMap(topics, interviewsByType);
+
         model.addAttribute("categoryId", categoryId);
         model.addAttribute("topics", topics);
+        model.addAttribute("new_interviews", interviewsByType);
+        model.addAttribute("map", interviewsByTopicMap);
         String categoryName = topics.isEmpty() ? "" : topics.get(0).getCategory().getName();
         RequestResponseTools.addAttrBreadcrumbs(model,
                 "Главная", "/index",
@@ -57,5 +68,16 @@ public class TopicsControl {
             log.error("Remote application not responding. Error: {}. {}, ", e.getCause(), e.getMessage());
         }
         return "topic/topics";
+    }
+    private Map<Integer, List<InterviewDTO>> interviewsByTopicMap(
+            List<TopicDTO> topics, List<InterviewDTO> interviews) {
+        Map<Integer, List<InterviewDTO>> map = new HashMap<>();
+        for (TopicDTO topicDTO : topics) {
+            map.put(
+                    topicDTO.getId(),
+                    interviews.stream().filter(i -> i.getId() == topicDTO.getId()).toList()
+            );
+        }
+        return map;
     }
 }
